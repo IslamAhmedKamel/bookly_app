7e26a701ab5bfbfc0a1aac4fde2ec850abef09d0
islam ahmed <islamahmedkamel592@gmail.com>
2026-05-21 19:31:36 +0300

    create text animation

--- lib/core/utils/constant.dart
+++ lib/core/utils/constant.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 abstract class ConstantHelper {
   static const kPrimaryFont = "Montserrat";
   static const kSecondFont = "PlayfairDisplay";
+  static const duration = Duration(seconds: 2);
   static const kPrimaryColor = Color(0xff000000);
 }
--- lib/features/splash/presentation/views/widgets/splash_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_body.dart
@@ -1,9 +1,36 @@
 import 'package:bookly_app/core/utils/assets_helper.dart';
-import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/core/utils/constant.dart';
+
+import 'package:bookly_app/features/splash/presentation/views/widgets/text_animation.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _slidingAnimation;
+  @override
+  void initState() {
+    initTextAnimation();
+
+    // Future.delayed(ConstantHelper.duration, () {
+    //   // ignore: use_build_context_synchronously
+    //   Navigator.pushReplacementNamed(context, HomeView.homeId);
+    // });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +38,23 @@ class SplashViewBody extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Image.asset(AssetsHelper.logoImage),
-        Align(child: Text("Read Free Books", style: StylesHelper.textStyle20)),
+        TextAnimation(
+          slidingAnimation: _slidingAnimation,
+          controller: _controller,
+        ),
       ],
     );
+  }
+
+  void initTextAnimation() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: ConstantHelper.duration,
+    );
+    _slidingAnimation = Tween<Offset>(
+      begin: Offset(0, 2),
+      end: Offset.zero,
+    ).animate(_controller);
+    _controller.forward();
   }
 }
