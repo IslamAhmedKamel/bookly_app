cfcd11a74903063583603904674cbb8e21ef6d1e
islam ahmed <islamahmedkamel592@gmail.com>
2026-05-22 14:01:28 +0300

    create statse home cubit

--- lib/features/home/data/cubit/cubit/get_books_cubit.dart
+++ lib/features/home/data/cubit/cubit/get_books_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_app/features/home/presentation/view_model/book_model.dart';
 import 'package:meta/meta.dart';
 
 part 'get_books_state.dart';
--- lib/features/home/data/cubit/cubit/get_books_state.dart
+++ lib/features/home/data/cubit/cubit/get_books_state.dart
@@ -4,3 +4,17 @@ part of 'get_books_cubit.dart';
 sealed class GetBooksState {}
 
 final class GetBooksInitial extends GetBooksState {}
+
+final class GetBooksSuccese extends GetBooksState {
+  final List<BookModel> booskList;
+
+  GetBooksSuccese({required this.booskList});
+}
+
+final class GetBooksFailure extends GetBooksState {
+  final String errMessage;
+
+  GetBooksFailure({required this.errMessage});
+}
+
+final class GetBooksLoading extends GetBooksState {}
